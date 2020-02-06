a621688b3ec9070eadc83d7b90cb358b6afd05cd
lucasgdiez <lucasgdiez@gmail.com>
2020-02-05 21:08:21 -0300

    adding prisma! removing old stuff

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const { GraphQLServer } = require("graphql-yoga");
 const resolvers = {
   Query: {
     info: () => "This is the API of a HackerNews Clone",
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
     link: (parent, args) => {
       return links.find((data) => {
         return data.id === args.id;
@@ -11,15 +13,20 @@ const resolvers = {
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
+    post: (root, args, context) => {
+      // const link = {
+      //   id: `link-${idCount++}`,
+      //   description: args.description,
+      //   url: args.url
+      // };
 
-      links.push(link);
-      return link;
+      // links.push(link);
+      // return link;
+
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     },
     updateLink: (parent, args) => {
       const newLink = {
